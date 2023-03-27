5c6a4ef29efb8f2a505de062ea7a735ae2b76232
rahul220300 <rahularajput2000@gmail.com>
2023-03-27 14:21:36 +0530

    adding second file

---
+++ demo.js
@@ -0,0 +1,33 @@
+// console.dir(document);
+// console.log(document.URL);
+// console.log(document.title);
+// document.title=123;
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+// console.log(document.all[10]);
+// document.all[10].textContent="Hello";
+// console.log(document.forms[0]);
+// console.log(document.links);
+// console.log(document.images);
+
+//getElemntId Method
+
+// console.log(document.getElementById("header-title"));
+ //var headerTitle=document.getElementById("header-title");
+ var header = document.getElementById("main-header");
+// headerTitle.textContent="Hello";
+// headerTitle.innerText="GoodBye";
+header.style.borderBottom="solid 3px #000";
+    
+var titlepage = document.getElementsByClassName("list-group");
+
+titlepage[0].style.fontWeight="bold";
+titlepage[0].style.color="green";
+
+
+
+
+
+
