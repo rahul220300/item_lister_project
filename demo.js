ef0c6d980a86a06e968b62c802375da7a462a4a1
rahul220300 <rahularajput2000@gmail.com>
2023-03-28 13:35:12 +0530

    change the element tage

--- demo.js
+++ demo.js
@@ -27,10 +27,10 @@
 // titlepage[0].style.color="green";
 
 
-var items= document.getElementsByClassName("list-group-item");
-items[2].textcontent="Rahul";
-items[2].style.fontWeight="bold";
-items[2].style.backgroundColor="green";
-for(var i=0;i<items.length;i++){
-  items[i].style.fontWeight="bold";
+var li= document.getElementsByTagName("li");
+li[2].textcontent="Rahul";
+li[2].style.fontWeight="bold";
+li[2].style.backgroundColor="green";
+for(var i=0;i<li.length;i++){
+  li[i].style.fontWeight="bold";
 }
