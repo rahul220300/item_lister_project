98d6c112f0ca8681bc1551e3dfe10e07907b8f98
rahul220300 <rahularajput2000@gmail.com>
2023-03-28 21:45:29 +0530

    create element using dom

--- demo.js
+++ demo.js
@@ -46,10 +46,72 @@
 
 
 //querySelectorAll using
-var items2 = document.querySelectorAll(".list-group-item");
-items2[1].style.color="green";
+// var items2 = document.querySelectorAll(".list-group-item");
+// items2[1].style.color="green";
 
-var odd = document.querySelectorAll("li:nth-child(odd)");
-for(var i=0;i<odd.length;i++){
-    odd[i].style.backgroundColor="green";
-}
+// var odd = document.querySelectorAll("li:nth-child(odd)");
+// for(var i=0;i<odd.length;i++){
+//     odd[i].style.backgroundColor="green";
+// }
+
+
+
+
+
+// //trversing the dom
+
+// var itemlist= document.querySelector("items");
+// //parentmode
+// console.log(itemlist.parentNode);
+// itemlist.parentNode.style.backgroundColor='#f4f4f4';
+// console.log(itemlist.parentNode.parentNode.parentNode);
+
+// //parentElement
+// console.log(itemlist.parentElement);
+// itemlist.parentElement.style.backgroundColor='#f4f4f4';
+// console.log(itemlist.parentElement.parentElement.parentElement);
+
+// //childNode
+
+// console.log(itemlist.childNodes);
+// itemlist.children[1].style.backgroundColor='#yellow';
+// console.log(itemlist.children[1]);
+
+// //firstChild
+// console.log(itemlist.firstChild);
+// itemlist.firstElementChild.textContent='Hello1';
+// console.log(itemlist.firstElementChild);
+
+// //lastChild
+// console.log(itemlist.lastChild);
+// itemlist.lastElementChild.textContent='Hello1';
+// console.log(itemlist.lastElementChild);
+
+
+// //NextSibling
+
+// console.log(itemlist.nextSibling);
+// itemlist.nextElementSibling.textContent='Hello1';
+// console.log(itemlist.nextElementSibling);
+
+// // previousSibling
+// console.log(itemlist.previousSibling);
+// itemlist.previousElementSibling.textContent='Hello1';
+// console.log(itemlist.previousElementSibling);
+
+
+//createElement
+
+
+var newhead = document.createElement('div');
+newhead.className='hello';
+newhead.id='hello1';
+newhead.setAttribute('title','hello head');
+var newHeadText = document.createTextNode("Hello");
+newhead.appendChild(newHeadText); 
+
+//above items lister 
+var container = document.querySelector('header .container');
+var h1 = document.querySelector('header h1');
+console.log(newhead);
+container.insertBefore(newhead,h1);
