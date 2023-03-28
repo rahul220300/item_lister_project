14f5cc600a031969c6506fa6703e7e800da2ddf7
rahul220300 <rahularajput2000@gmail.com>
2023-03-28 14:16:48 +0530

    QuerySelectorAll using

--- demo.js
+++ demo.js
@@ -35,7 +35,21 @@
 //   li[i].style.fontWeight="bold";
 // }
 
-var items = document.querySelector(".list-group-item:nth-child(2)");
-items.style.backgroundColor="green";
-var visible = document.querySelector(".list-group-item:nth-child(3)");
-visible.style.visibility="hidden";
+
+//QuerySelctor using
+
+// var items = document.querySelector(".list-group-item:nth-child(2)");
+// items.style.backgroundColor="green";
+// var visible = document.querySelector(".list-group-item:nth-child(3)");
+// visible.style.visibility="hidden";
+
+
+
+//querySelectorAll using
+var items2 = document.querySelectorAll(".list-group-item");
+items2[1].style.color="green";
+
+var odd = document.querySelectorAll("li:nth-child(odd)");
+for(var i=0;i<odd.length;i++){
+    odd[i].style.backgroundColor="green";
+}
